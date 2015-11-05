124758ef6e144e930d0b252bd6ad5f3b3b5729cd
Teo <zeni.matteo@gmail.com>
2015-11-05 10:29:18 +0100

    something to fix

--- app.js
+++ app.js
@@ -8,6 +8,40 @@
  *    word_de : "Apfel"   // word in german
  *  }
  */ 
+/*
+function reset(){
+  $(".done").addClass("hidden")
+ $(".done").removeClass("done")
+ $(".done").addClass("hidden")
+ $(".done").removeClass("done")
+}*/
+var tot= 0;
+var yes= 0;
+var no= 0;
+function fertig(){
+    var final = "<h3> You remembered <span id='tot-good'>YES</span> "+"of <span id='tot'>TOT</span></h3>";
+    
+            final = final.replace("YES", yes);
+            final = final.replace("TOT", tot);
+            $(".final").append(final);
+}
+
+function correct(){
+    yes++;
+    tot++;
+    if(tot ==7)
+    {
+        fertig();
+    }
+}
+function incorrect(){
+    no++;
+    tot++;
+    if(tot ==7)
+    {
+        fertig();
+    }
+}
 
 var tmpl = ' <li id="ID">' +
            '  <h3>WORD</h3>' +
@@ -16,11 +50,23 @@ var tmpl = ' <li id="ID">' +
 
 $(document).ready(function(){
 
+    $(".final").addClass("hidden")
+    //$(".done").removeClass("done")
+    
+    for( var i=0; i < data.length; i++) {
+        var infos = tmpl;
+            infos = infos.replace("ID", i);
+            infos = infos.replace("WORD", data[i]);
+            infos = infos.replace("SOLUTION", data[i]);
+            $(".cards").append(infos);
+    }
+    
 
 });
 
 
-
+$(".opt-correct").click(correct());
+$(".opt-incorrect").click(incorrect());
 
 
 
